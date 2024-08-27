369a339d8d9559b9370ca2d71682500a4cfc2cbe
catloverrrr <70571231+catloverrrr@users.noreply.github.com>
2024-08-27 09:01:33 +0900

    Add files via upload

---
+++ design.js
@@ -0,0 +1,15 @@
+document.getElementById("basicButton").addEventListener("click", function() {
+    window.location.href = "basic.html";
+});
+
+document.getElementById("investmentButton").addEventListener("click", function() {
+    window.location.href = "investment.html";
+});
+
+document.getElementById("researchButton").addEventListener("click", function() {
+    window.location.href = "research.html";
+});
+
+document.getElementById("visionButton").addEventListener("click", function() {
+    window.location.href = "vision.html";
+});
